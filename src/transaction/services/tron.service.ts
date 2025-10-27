ad5976b331e1b425708e2b6903fb180ad858ca12
Erlan-Bam <erlanzh.gg@gmail.com>
2025-10-27 16:07:12 +0500

    feat: updated

--- src/transaction/services/tron.service.ts
+++ src/transaction/services/tron.service.ts
@@ -33,14 +33,15 @@ export class TransactionTronService {
 
   async getWalletUSDTTransactions(address: string) {
     try {
-      const now = Math.floor(Date.now() / 1000);
+      const now = Date.now();
+      const start = now - 22 * 60 * 60 * 1000;
       const response = await this.tron.get('/api/token_trc20/transfers', {
         params: {
           start: 0,
           limit: 50,
           relatedAddress: address,
           contract_address: this.USDT_CONTRACT_ADDRESS,
-          start_timestamp: now - 10 * 60 * 1000,
+          start_timestamp: start,
           end_timestamp: now,
         },
       });
